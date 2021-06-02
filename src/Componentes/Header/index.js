9be424ba500d2af39ac47738aa94acb832227f79
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2021-06-01 22:25:34 -0300

    Subindo projeto.

---
+++ src/Componentes/Header/index.js
@@ -0,0 +1,11 @@
+import react from 'react';
+import './style.css';
+
+export default function Header()
+{
+    return (
+    <header>
+        Administração de Filmes
+    </header>
+    );
+}
---
+++ src/Componentes/HomePage/index.js
@@ -0,0 +1,12 @@
+import react from 'react';
+import './style.css';
+
+export default function HomePage()
+{
+    return (
+        <div className="home-page">
+            teste
+        </div>
+
+    );
+}
